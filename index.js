a83b2d67d4142befddb2131bc040762b822a0e16
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-05-09 00:07:07 +0800

    Consistency

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var mdns = require('multicast-dns')
 var events = require('events')
 
 module.exports = function (opts) {
-  if (!opts) opts = {
-    interval: 5000
+  if (!opts) {
+    opts = {
+      interval: 5000
+    }
   }
 
   var dns = mdns()
@@ -26,45 +28,36 @@ module.exports = function (opts) {
       browser.emit('serviceUp', service)
     }
 
+    var serviceDown = function (service, key) {
+      browser.emit('serviceDown', service)
+      delete services[key]
+    }
+
     var query = function () {
       debug('query', type)
       dns.query(type, 'PTR')
     }
 
-    browser.stop = function () {
-      clearInterval(browserInterval)
-      clearInterval(cleanerInterval)
-    }
-
-    browser.start = function () {
-      this.cleaner()
-      browserInterval = setInterval(query, opts.interval)
-      query()
-    }
-
-    browser.cleaner = function () {
+    var cleaner = function () {
       var clean = function () {
         Object.keys(services).forEach(function (key) {
           var service = services[key]
           if (Date.now() - service.lastSeen > opts.interval) {
-            browser.emit('serviceDown', service)
-            delete services[key]
+            serviceDown(service, key)
           }
         })
-
       }
 
       cleanerInterval = setInterval(clean, opts.interval)
       clean()
     }
 
-    dns.on('response', function (response) {
-      // response.answers.forEach(function (a) {
-      //   if (a.type === 'PTR' || a.name === type) {
-      //     var name = a.data.replace('.' + type, '')
-      //   }
-      // })
+    var discover = function () {
+      browserInterval = setInterval(query, opts.interval)
+      query()
+    }
 
+    dns.on('response', function (response) {
       var onanswer = function (a) {
         var name
 
@@ -97,6 +90,16 @@ module.exports = function (opts) {
       response.answers.forEach(onanswer)
 
     })
+
+    browser.stop = function () {
+      clearInterval(browserInterval)
+      clearInterval(cleanerInterval)
+    }
+
+    browser.start = function () {
+      discover()
+      cleaner()
+    }
 
     return browser
 
